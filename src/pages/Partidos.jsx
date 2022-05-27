b6cf1d709be9b5406fbcfd98618fe2e1cf67e237
Alfonso Cano <alfonsoheartagram@gmail.com>
2022-05-27 00:25:14 -0500

    Al Fin

--- src/pages/Partidos.jsx
+++ src/pages/Partidos.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Partidos = () => {
+  const [partidos, setPartidos] = useState([])
+
+    useEffect(() => {
+        // axios.get(`${process.env.REACT_APP_SERVER_URL}/perfil/equipos`)
+        axios.get(`http://localhost:5005/partidos`)
+        .then(datos => setPartidos(datos.data.matches))
+        .catch(error => {console.log("Este es el error =>", error)})
+    }, [])
+
+
   return (
-    <div>Partidos</div>
+    <div className='contenedor'>
+        {
+           partidos.map(partido => {
+                return (
+                    <div>
+                        Partido
+                        {
+                            partido.map((equipo) => <p>{equipo.nombreDelEquipo}</p>)
+                        }
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
